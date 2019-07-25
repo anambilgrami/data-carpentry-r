64e0475bbbef0519472c401ff51e9bd36615d205
MISS Anam Bilgrami <40743381@students.mq.edu.au>
2019-07-25 15:48:43 +1000

    Functions

--- scripts/introductiontoR.R
+++ scripts/introductiontoR.R
@@ -11,10 +11,15 @@ print("hello,world")
 area_hectares <- 2.5 #land area in hectares
 area_acres<- 2.47*area_hectares #convert to acres
 area_acres
+area
 
 (length  <- 5)
 (width <- 5)
 area <- length*width
+
+# b <- sqrt(x = a)
+
+round(x=3.14159, digits=2)
 
 
 
