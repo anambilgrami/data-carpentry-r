7fa975c5db89184f3c64794a2b18fe8a8019babf
MISS Anam Bilgrami <40743381@students.mq.edu.au>
2019-07-25 15:37:14 +1000

    Area formula

--- scripts/introductiontoR.R
+++ scripts/introductiontoR.R
@@ -1,6 +1,20 @@
 # load required packages
-# install.packages(tidyverse)
+install.packages("tidyverse")
 
 library(tidyverse)
 
 print("hello,world")
+
+(area_hectares <- 1.0)
+1.0 -> area_hectares
+
+area_hectares <- 2.5 #land area in hectares
+area_acres<- 2.47*area_hectares #convert to acres
+area_acres
+
+(length  <- 5)
+(width <- 5)
+area <- length*width
+
+
+
