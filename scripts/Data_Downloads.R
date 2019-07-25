dc8c3e611016b5bf1e7172f17b2c7e675f7110ce
MISS Anam Bilgrami <40743381@students.mq.edu.au>
2019-07-25 12:21:11 +1000

    Download new datasets

--- scripts/Data_Downloads.R
+++ scripts/Data_Downloads.R
@@ -1,3 +1,7 @@
 # Download datasets
 
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_messy.xlsx", destfile = "./data/SAFI_messy.xlsx", mode = "wb")
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", destfile = "./data/SAFI_clean.csv")
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.csv", destfile = "./data/SAFI_dates.csv", mode="wb")
